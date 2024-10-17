296a1c36764c87319a74de8e0017bb64678fb1f1
Carlos Eduardo <cemluna2021@gmail.com>
2024-10-16 20:22:12 -0600

    Reporte de Sonarqube

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,67 +1,65 @@
 <template>
     <section class="featured-job-area feature-padding min-vh-100 d-flex flex-column justify-content-center align-items-center">
-        <div class="container h-100 d-flex flex-column justify-content-center">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="section-tittle text-center mb-5">
-                        <h2>Empleos publicados</h2>
-                    </div>
-                </div>
+      <div class="container h-100 d-flex flex-column justify-content-center">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="section-tittle text-center mb-5">
+              <h2>Empleos publicados</h2>
             </div>
-            <div class="row justify-content-center">
-                <div v-for="(job, index) in jobs" :key="index" class="col-xl-10 col-lg-10 col-md-10 col-sm-12 mb-4 d-flex justify-content-center">
-                    <JobItem :job="job" />
-                </div>
-            </div>
+          </div>
         </div>
+        <div class="row justify-content-center">
+          <div v-for="(job, index) in jobs" :key="index" class="col-xl-10 col-lg-10 col-md-10 col-sm-12 mb-4 d-flex justify-content-center">
+            <JobItem :job="job" />
+          </div>
+        </div>
+      </div>
     </section>
-</template>
-
-<script>
-import JobItem from './JobItem.vue';
-
-export default {
-    name: "JobList",
-    components: {
-        JobItem
-    },
-    props: {
-        jobs: {
-            type: Array,
-            required: true
-        }
+  </template>
+  
+  <script setup>
+  import JobItem from './JobItem.vue';
+  import { defineProps } from 'vue';
+  
+  // Definir las propiedades del componente
+  const props = defineProps({
+    jobs: {
+      type: Array,
+      required: true
     }
-};
-</script>
-
-<style scoped>
-.featured-job-area {
+  });
+  props.value
+  </script>
+  
+  <style scoped>
+  .featured-job-area {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     text-align: center; /* Asegura que el texto esté centrado */
-}
-
-.section-tittle h2 {
+  }
+  
+  .section-tittle h2 {
     font-size: 2.5rem;
     color: #0c0c0c;
-}
-
-.row {
+  }
+  
+  .row {
     width: 100%;
-}
-
-.col-xl-10, .col-lg-10, .col-md-10, .col-sm-12 {
+  }
+  
+  .col-xl-10, .col-lg-10, .col-md-10, .col-sm-12 {
     display: flex;
     justify-content: center;
-}
-
-.container {
+  }
+  
+  .container {
     max-width: 1200px;
-}
-
-.min-vh-100 {
+  }
+  
+  .min-vh-100 {
     min-height: 100vh;
-}
-</style>
+  }
+  </style>
+  
