5eb7f3be69858c6b640f3edc42da74a3df9fc69a
Carlos Eduardo <cemluna2021@gmail.com>
2024-11-04 20:30:14 -0600

    Otro cambio

--- src/views/PostJobView.vue
+++ src/views/PostJobView.vue
@@ -3,7 +3,7 @@
     <NavbarComponent />
   </header>
 
-  <div class="container-fluid">
+  <div class="caja container-fluid">
     <!-- Información de la empresa -->
     <div class="row">
       <PostJobHeader @sendCompanyData="receiveCompanyData" />
@@ -22,8 +22,9 @@
         <SelectComponent :modelValue="facultad" :required=true :options="facultades" />
       </div>
       <div class="col-xl-4 col-lg-4 col-md-4">
-        <h6>Salario $</h6>          
-        <input class="form-control" type="number" placeholder="400" min="400" step="50" v-model="salarie" required /><br>
+        <h6>Salario $</h6>
+        <input class="form-control" type="number" placeholder="400" min="400" step="50" v-model="salarie"
+          required /><br>
       </div>
     </div>
     <div class="row mt-2 text-start">
@@ -32,42 +33,42 @@
         <textarea class="form-control" rows="3" placeholder="Descripción" v-model="description" required></textarea>
       </div>
     </div>
-    <div class="row mt-3"><h3>Información Adicional</h3></div>
-    
+    <div class="row mt-3">
+      <h4>Información Adicional</h4>
+    </div>
     <div class="row mt-3 text-start">
-      
-      <div class="col-6">
-        <h6>Duración</h6>
-        <div class="col -6">
-          <SelectComponent :modelValue="periodoDeTiempo" :required="true" :options="SMA"  @update:modelValue="updatePeriodoDeTiempo" />
-        </div>
-        <div class="col -6">
-          <input class="form-control" type="number" placeholder="0" min="0" step="1" v-model="time" :disabled="periodoDeTiempo === 'Ninguna'" />
-        </div>
+      <h6>Duración</h6>
+      <div class="col">
+        <SelectComponent :modelValue="periodoDeTiempo" :required="true" :options="SMA"
+          @update:modelValue="updatePeriodoDeTiempo" />
+      </div><br>
+      <div class="col">
+        <input class="form-control" type="number" placeholder="0" min="0" step="1" v-model="time"
+          :disabled="periodoDeTiempo === 'Ninguna'" />
       </div>
-      <div class="col-6">
-        <h6>Lugar de trabajo</h6>
-        <div class="col -6">
+    </div>
+    <div class="row mt-3 text-start"> 
+      <h6>Lugar de trabajo</h6>       
+        <div class="col">
           <select class="form-select form-control" v-model="departamento" @change="seleccionarDistritos">
             <option selected disabled>Departamento</option>
             <option v-for="(distritos, dept) in departamentos" :key="dept">{{ dept }}</option>
           </select>
         </div><br>
-        <div class="col -6">
+        <div class="col">
           <select class="form-select form-control" v-model="distrito" required>
             <option selected disabled>Distrito</option>
             <option v-for="distrito in distritosDisponibles" :key="distrito">{{ distrito }}</option>
           </select>
         </div>
-      </div>
     </div>
     <div class="row mt-3 text-start">
       <div class="col -6">
-      <h6>Modalidad</h6>
+        <h6>Modalidad</h6>
         <SelectComponent :modelValue="modoEmpleo" :required=true :options="modalidadesEmpleo" />
       </div>
       <div class="col -6">
-      <h6>Tipo de empleo</h6>
+        <h6>Tipo de empleo</h6>
         <SelectComponent :modelValue="tipoEmpleo" :required=true :options="tiposEmpleo" />
       </div>
     </div>
@@ -108,7 +109,7 @@ let facultades = [
   "Ciencias de la salud", "Derecho y relaciones internacionales", "Ciencias empresariales",
   "Ciencia y tecnología", "Ciencias y humanidades", "Ingenería y arquitectura"
 ]
-const SMA = ["Ninguna","Semanas", "Meses", "Años"]
+const SMA = ["Ninguna", "Semanas", "Meses", "Años"]
 const modalidadesEmpleo = ["Presencial", "Virtual", "Semi Presencial"]
 const tiposEmpleo = ["Tiempo completo", "Medio tiempo", "Freelance"]
 const departamentos = {
@@ -133,10 +134,10 @@ const seleccionarDistritos = () => {
 
 // Función para actualizar periodoDeTiempo
 const updatePeriodoDeTiempo = (value) => {
-    periodoDeTiempo.value = value;
-    if (value === "Ninguna") {
-        time.value = 0; // Reiniciar time si se selecciona "Ninguna"
-    }
+  periodoDeTiempo.value = value;
+  if (value === "Ninguna") {
+    time.value = 0; // Reiniciar time si se selecciona "Ninguna"
+  }
 };
 
 // Función para obtener datos de la empresa desde Firestore
@@ -228,5 +229,9 @@ onMounted(() => {
   padding: 10px;
   font-size: 1em;
   border-radius: 5px;
+}
+
+.caja{
+  max-width: 80em;
 }
 </style>
