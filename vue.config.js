39ebedb9e9a3c06dadad38545592870cdcd2f85b
luis barrera <109974057+YoSoyLuis503@users.noreply.github.com>
2024-11-25 20:14:00 -0600

    cambios al vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/La-Gran-Coalicion-Edicion-Empresarial/'  // Cambia esto por el nombre de tu repositorio
+    : '/'
 })
