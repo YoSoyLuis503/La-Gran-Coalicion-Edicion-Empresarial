fe5331e22fdcd774a64e8144526827f148bd819d
Carlos Eduardo <cemluna2021@gmail.com>
2024-12-04 10:09:40 -0600

    Titulo y descripcion vista candidatos

--- src/views/CandidatesView.vue
+++ src/views/CandidatesView.vue
@@ -14,7 +14,10 @@
                     </div>
                 </div>
                 <div class="row text-start">
-                    <AboutJob />
+                    <div class="mt-4">
+                        <h2>{{ title }}</h2>
+                        <p>{{ description }}</p>
+                    </div>
                 </div>
                 <div class="row">
                     <CandidateCard />
@@ -28,14 +31,44 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
 import NavbarComponent from '@/components/Navbar/NavbarComponent.vue'
 import SearchBar from '@/components/SearchBar.vue';
 import SelectComponent from '@/components/SelectComponent.vue';
-import AboutJob from '@/components/AboutJob.vue';
 import CandidateCard from '@/components/CandidateCard/CandidateCard.vue';
 import FooterComponent from '@/components/FooterComponent.vue';
+import { ref, onMounted } from 'vue';
+import { db } from '@/js/firebase';
+import { doc, getDoc} from 'firebase/firestore';
+import { useRoute} from 'vue-router';
 
 let filtros = ["Nombre (A-Z)", "Nombre (Z-A)", "Edad M-m", "Edad m-M", "Más reciente", "Más antiguo", "Favoritos"]
 let filtro = ref('Más reciente')
+
+const route = useRoute()
+const jobId = route.params.jobId
+console.log(jobId)
+
+const title = ref("")
+const description = ref("")
+
+// Función para obtener los datos del empleo desde Firestore
+const fetchJobData = async () => {
+    if (jobId) {
+        const jobRef = doc(db, "empleos", jobId);
+        const jobSnap = await getDoc(jobRef);
+
+        if (jobSnap.exists()) {
+            const jobData = jobSnap.data();
+            title.value = jobData.Título;
+            description.value = jobData.Descripción;
+        } else {
+            console.log("No se encontró el empleo.");
+        }
+    }
+};
+
+// Llama a la función para cargar los datos al montar el componente
+onMounted(() => {
+    fetchJobData();
+});
 </script>
